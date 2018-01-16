520c557cca318b273ebc6562931c951568a669a5
keastway <kadin.eastway2021@gmail.com>
2018-01-16 15:48:46 -0500

    Working on the menu button and creating a new project for it to open to.

--- app/src/main/java/org/mcbain/scouting2018/MainActivity.java
+++ app/src/main/java/org/mcbain/scouting2018/MainActivity.java
@@ -1,7 +1,9 @@
 package org.mcbain.scouting2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void OpenMenu(View view) {
+        Intent intent = new Intent(this, DisplayMenu.class);
+        startActivity(intent);
     }
 }
