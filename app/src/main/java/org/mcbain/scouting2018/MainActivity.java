412be46735b293946b37bab19aef3ca4ef5e72bf
enelson <e.nelson@mcbain.org>
2018-01-22 15:51:40 -0500

    Dynamically creates teams

--- app/src/main/java/org/mcbain/scouting2018/MainActivity.java
+++ app/src/main/java/org/mcbain/scouting2018/MainActivity.java
@@ -1,7 +1,10 @@
 package org.mcbain.scouting2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static org.mcbain.scouting2018.Globals.teams;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teams = new Team[2];
+        teams[0]= new Team(6066, "RamblerRobotics");
+        teams[1]= new Team(2323, "SuperSmashClash");
+        startActivity(new Intent(this, TeamList.class));
     }
 }
