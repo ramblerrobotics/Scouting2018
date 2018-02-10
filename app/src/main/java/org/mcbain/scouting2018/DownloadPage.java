abd991dfdb6d62fe4590b8ad7f3a60972800a711
Reggie McLean <reggieman00@gmail.com>
2018-02-10 14:11:20 -0500

    Downloads and sets match schedule.

--- app/src/main/java/org/mcbain/scouting2018/DownloadPage.java
+++ app/src/main/java/org/mcbain/scouting2018/DownloadPage.java
@@ -31,5 +31,24 @@ public class DownloadPage {
             return ret;
         }catch(Exception e){return "FAIL";}
     }
+    public static String downloadSched(String event){
 
+        try {
+            URL url = new URL(" http://www.thebluealliance.com/api/v3/event/" + event + "/matches/simple?X-TBA-Auth-Key=KAtHgAsYQgrSCUKHcl7jt0iRHjen2BFcYtzExuffElkV8lOHen2nPY2NXyYbnjLm");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            InputStream is;
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String ret = "";
+            try {
+                String tmp2;
+                while (!(tmp2 = br.readLine()).equals("]")) {
+                    ret = ret.concat(tmp2 + "\n");
+                }
+            } catch (Exception error_variable) {
+            }
+            return ret;
+        }catch(Exception e){return "FAIL";}
+    }
 }
--- app/src/main/java/org/mcbain/scouting2018/MainActivity.java
+++ app/src/main/java/org/mcbain/scouting2018/MainActivity.java
@@ -75,6 +75,81 @@ public class MainActivity extends AppCompatActivity {
                 }
             }catch(Exception e){
             }
+            //match schedule
+            Globals.matches = new Match[100];
+            for(int j = 0; j < 100; j++){
+                Globals.matches[j] = new Match();
+            }
+            String data2 = DownloadPage.downloadSched("2017mike2");
+            if(data2.equals("FAIL"))
+                return;
+            try{
+            String comp_level = "FAIL";
+            String match_number = "FAIL";
+            Boolean isBlue = false;
+            Boolean isRed = false;
+            Boolean loadNext = false;
+            String time = "FAIL";
+            String[] blue = new String[3];
+            String[] red = new String[3];
+            int loaded = 0;
+            int k = 0;
+            for(String line : data2.split("\n")){
+                if((isBlue || isRed) && !loadNext){
+                    if(line.contains("\"team_keys")){
+                        loadNext = true;
+                        continue;
+                    }
+                }
+                if(loadNext){
+                    if(isBlue){
+                        blue[loaded] = line.split("frc")[1].split("\"")[0];
+                        loaded++;
+                        if(loaded == 3) {
+                            loadNext = false;
+                            isBlue = false;
+                            loaded = 0;
+                        }
+                    }else{
+                        red[loaded] = line.split("frc")[1].split("\"")[0];
+                        loaded++;
+                        if(loaded == 3){
+                            loadNext = false;
+                            isRed = false;
+                            loaded = 0;
+                        }
+                    }
+                    continue;
+                }
+                if(line.contains("comp_level"))
+                    comp_level = line.split(": ")[1].split("\"")[1];
+                if(line.contains("match_number"))
+                    match_number = line.split(": ")[1].split(",")[0];
+                if(line.contains("predicted_time")) {
+                    time = line.split(": ")[1].split(",")[0];
+                    if(time.equals("null"))
+                        time = "1";
+                }
+                if(line.contains("\"red\":"))
+                    isRed = true;
+                if(line.contains("\"blue\":"))
+                    isBlue = true;
+                if(!comp_level.equals("FAIL") && !match_number.equals("FAIL") && !time.equals("FAIL") && blue[2] != null){
+                    if(comp_level.equals("qm")) {
+                        Globals.matches[k].init(Long.parseLong(time), Integer.parseInt(red[0]), Integer.parseInt(red[1]), Integer.parseInt(red[2]), Integer.parseInt(blue[0]), Integer.parseInt(blue[1]), Integer.parseInt(blue[2]), Integer.parseInt(match_number));
+                        k++;
+                    }
+                    comp_level = "FAIL";
+                    match_number = "FAIL";
+                    isBlue = false;
+                    isRed = false;
+                    loadNext = false;
+                    time = "FAIL";
+                    blue = new String[3];
+                    red = new String[3];
+                }
+            }}catch(Exception e){
+            }
             FileIO.save(getApplicationContext());
         }
     }
